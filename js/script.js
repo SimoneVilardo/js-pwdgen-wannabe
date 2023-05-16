1167cb79c669d518913aca53d401411ef26c0889
SimoneVilardo <simovila95@gmail.com>
2023-05-16 14:47:48 +0200

    Inserimento commenti

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
+// PROMT INSERIMENTO NOME
 let nome = prompt('Qual è il tuo nome?')
 
+// PROMT INSERIMENTO COGNOME
 let cognome = prompt('Qual è il tuo cognome?')
 
+// PROMT INSERIMENTO COLORE PREFERITO
 let colore_preferito = prompt('Qual è il tuo colore preferito?')
 
+// VARIABILE NUMERO
 let numero = '21'
 
+// VARIABILI UNIFICATE
 let password = (nome + cognome + colore_preferito + numero)
 
+// COME APPARE IN PAGINA
 document.getElementById('password').innerHTML = password
